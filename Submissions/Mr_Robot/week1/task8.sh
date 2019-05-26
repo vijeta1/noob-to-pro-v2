7234557d2d32fd9d45ba4d856b2f00568153159b
blackpearl <33037393+vijeta1@users.noreply.github.com>
2019-05-26 11:08:34 +0530

    Delete task8.sh

--- Submissions/Mr_Robot/week1/task8.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-ssh -R any_port_for_listening:localhost:22 sourceuser@source_ip
-ssh user@machine_ip "echo Hi\!"
-scp -r /path1/folder user@remotehost:/path2/week1
-sftp user@server_ip
-export http_proxy="http://USER:PASSWORD@PROXY_SERVER:PORT"
-export https_proxy="https://USER:PASSWORD@PROXY_SERVER:PORT"
-export ftp_proxy="http://USER:PASSWORD@PROXY_SERVER:PORT"
