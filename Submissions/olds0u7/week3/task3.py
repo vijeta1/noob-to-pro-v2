786e5276f13f660b3f6d4a5dcc5db01a8ec556a0
vaibhav-1207 <43417166+vaibhav-1207@users.noreply.github.com>
2019-06-15 17:33:34 +0530

    Create task3.py

---
+++ Submissions/olds0u7/week3/task3.py
@@ -0,0 +1,15 @@
+import requests
+import json
+
+page=requests.get('https://jsonplaceholder.typicode.com/comments')
+
+data=page.json()
+
+a=[]
+
+for i in data:
+  if i['email'] not in a:
+    a.append(i['email'])
+    
+for j in a:
+  print(j)
