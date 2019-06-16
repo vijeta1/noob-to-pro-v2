2857d476aecdf5fdd56c4cdda2ad858963a68cc0
jishansingh <singhjishan2@gmail.com>
2019-06-17 00:12:05 +0545

    submit week4

---
+++ Submissions/ringmaker/week4/task9/task9.php
@@ -0,0 +1,31 @@
+<?php
+$target_URL="task9.xml";
+$html = new DOMDocument();
+@$html->loadHTMLFile($target_URL);
+$login = simplexml_load_file($target_URL);
+if(isset($_POST['user'])){
+	$user=$_POST['user'];
+	$pass=$_POST['pass'];
+	$isuser=0;
+	$result=$login->xpath("/company/employee[user='$user']");
+	foreach($result as $employ) {
+        	echo "hello {$employ->firstname}<br />";
+		exit();
+    	}
+	echo("username or password incorrect");
+	exit();
+}
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+
+<form method="post">
+	<input type="text" name="user">
+	<input type="text" name="pass">
+	<input type="submit" value="check">
+</body>
+</html>
